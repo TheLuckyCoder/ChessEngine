9a6ef254c99a57b1030d080d34ec2c5bd7754f75
Razvan <razvan.filea@gmail.com>
2020-06-05 14:18:52 +0300

    New Piece Icons

--- ChessAndroid/app/src/main/java/net/theluckycoder/chess/model/BoardAppearance.kt
+++ ChessAndroid/app/src/main/java/net/theluckycoder/chess/model/BoardAppearance.kt
@@ -1,9 +1,10 @@
 package net.theluckycoder.chess.model
 
-data class ChessColors(
+data class BoardAppearance(
     val whiteTile: Int,
     val blackTile: Int,
     val possible: Int,
     val lastMoved: Int,
-    val kingInCheck: Int
+    val kingInCheck: Int,
+    val showCoordinates: Boolean
 )
