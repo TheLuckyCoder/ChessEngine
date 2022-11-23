da3bdaaebbbf790f06cfc59c9491e1b0919f03f8
Razvan Filea <razvan.filea@gmail.com>
2022-11-23 14:41:26 +0200

    Temporarily fix CI

--- ChessAndroid/wearos/src/main/java/net/theluckycoder/chess/wearos/ui/screen/WatchScreen.kt
+++ ChessAndroid/wearos/src/main/java/net/theluckycoder/chess/wearos/ui/screen/WatchScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.Button
@@ -179,7 +178,6 @@ private fun WatchChessBoard(
         PromotionDialog(showPromotionDialog)
 }
 
-@PreviewParameter
 @Composable
 private fun PromotionDialog(showPromotionDialog: MutableState<List<Move>>) {
     val promotionResources = remember {
@@ -195,7 +193,7 @@ private fun PromotionDialog(showPromotionDialog: MutableState<List<Move>>) {
             Text(stringResource(id = R.string.promotion_choose_piece))
         },
     ) {
-        TODO()
+//        TODO()
         piecesPainters.forEachIndexed { index, painter ->
             Button(
                 colors = ButtonDefaults.secondaryButtonColors(),
